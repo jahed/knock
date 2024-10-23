850f8fc20a43131a506fd6a84833f69341f02162
jahed.dev <3330963+jahed@users.noreply.github.com>
2024-10-23 17:14:38 +0100

    docs: add powershell get-help comments

--- knock.ps1
+++ knock.ps1
@@ -1,3 +1,38 @@
+<#
+.SYNOPSIS
+Given a host, looks up its port sequence and knocks on it.
+
+.DESCRIPTION
+Given a host, looks up its port sequence and knocks on it.
+
+Version:   v2.2.1
+Homepage:  https://github.com/jahed/knock
+Donate:    https://jahed.dev/donate
+
+.PARAMETER TargetHost
+Target host. Where you want to knock. Must exist in config file.
+
+.PARAMETER ConfigFile
+Config file location. Defaults to ~/.config/knock/hosts.json.
+
+.INPUTS
+None.
+
+.OUTPUTS
+None.
+
+.EXAMPLE
+PS> .\knock.ps1 -ConfigFile .\knock.json example.com
+
+Will knock on example.com at ports 1000 then 2000 then 3000 given the config file:
+
+{
+  "example.com": ["1000", "2000", "3000"]
+}
+
+.LINK
+https://github.com/jahed/knock
+#>
 param( 
   [Parameter(Mandatory=$true,Position=0)] 
   [String] $TargetHost
